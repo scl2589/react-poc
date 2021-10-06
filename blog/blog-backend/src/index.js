946bd396bc900f319dcd4185a8eaf3898e08c0f7
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-06 16:30:38 +0900

    Add router to posts

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,47 +1,21 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser')
+
+const api = require('./api')
 
 const app = new Koa();
-const router = new Router();
+const router = new Router(); 
 
 // 라우터 설정
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
+router.use('/api', api.routes()); // api 라우트 적용 
 
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  ctx.body = id ? `포스트 ${id}}` : '포스트 아이디가 없습니다.';
-});
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-// app.use(async (ctx, next) => {
-//   console.log(ctx.url);
-//   console.log(1);
-//   if (ctx.query.authorized !== '1') {
-//     ctx.status = 401; // Unauthorized
-//     return;
-//   }
-//   await next();
-//   console.log('END');
-// });
-
-// app.use((ctx, next) => {
-//   console.log(2);
-//   next();
-// });
-
-// app.use((ctx) => {
-//   ctx.body = 'hello world';
-// });
+// app 인스턴스에 라우터 적용 
+app.use(router.routes()).use(router.allowedMethods()); 
 
 app.listen(4000, () => {
-  console.log('Listening to port 4000');
-});
+  console.log('Listening to port 4000')
+})
