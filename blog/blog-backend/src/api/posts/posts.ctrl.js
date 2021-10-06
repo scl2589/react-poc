9a25926035436a75a51cb45a0779e4603235f69e
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-07 08:59:52 +0900

    Add CRUD logic

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,3 +1,6 @@
+
+import Post from '../../models/post';
+
 let postId = 1; //id의 초깃값
 
 //posts 배열 초기 데이터 
@@ -10,41 +13,47 @@ const posts = [
 ]
 
 /* 포스트 작성 
-POST /api/psots 
+POST /api/posts 
 { title, body }
 */ 
 
-exports.write = ctx => {
+export const write = async ctx => {
     //REST API의 Request Body는 ctx.request.body에서 조회할 수 있다. 
-    const { title, body } = ctx.request.body; 
-    postId += 1; // 기존 postId 값에 1을 더한다. 
-    const post = {id: postId, title, body };
-    posts.push(posts);
-    ctx.body = post; 
+    const { title, body, tags } = ctx.request.body; 
+    const post = new Post({ title, body, tags });
+    try {
+        await post.save(); 
+        ctx.body = post; 
+    } catch(e) {
+        ctx.throw(500, e)
+    }
 }
 
-exports.list = ctx => {
-    ctx.body = posts; 
+export const list = async ctx => {
+    try {
+        const posts = await Post.find().exec(); 
+        ctx.body = posts; 
+    } catch(e) {
+        ctx.throw(500, e);
+    }
 }
 
 /* 특정 포스트 조회
 GET /api/posts/:id
 */
-exports.read = ctx => {
+export const read = async ctx => {
     const { id } = ctx.params;
-    // 주어진 id 값으로 포스트를 찾는다.
-    // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나 
-    // 비교할 p.id 값을 문자열로 변경해야 한다. 
-    const post = posts.find(p => p.id.toString() === id); 
-    // 포스트가 없으면 올류를 반환한다. 
-    if (!post) {
-        ctx.status = 404; 
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다. '
+    try {
+        const post = await Post.findById(id).exec(); 
+        // 포스트가 없으면 올류를 반환한다. 
+        if (!post) {
+            ctx.status = 404; 
+            return; 
         }
-        return; 
+        ctx.body = post 
+    } catch(e) {
+        ctx.throw(500, e)
     }
-    ctx.body = post 
 }
 
 
@@ -52,28 +61,21 @@ exports.read = ctx => {
  * 특정 포스트 제거 
  * DELETE /api/posts/:id 
  */
-exports.remove = ctx => {
+export const remove = async ctx => {
     const { id } = ctx.params; 
-    //해당 id를 가진 post가 몇 번째인지 확인한다. 
-    const index = posts.findIndex(p => p.id.toString() === id); 
-    // 포스트가 없으면 오류를 반환한다. 
-    if (index == -1) {
-        ctx.status = 404; 
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.'
-        }
-        return 
+    try {
+        await Post.findByIdAndRemove(id).exec(); 
+        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+    } catch(e) {
+        ctx.throw(500, e) 
     }
-    // index 번쨰 아이템을 제거한다. 
-    posts.splice(index, 1); 
-    ctx.status = 204; //No content 
 }
 
 /** 포스트 수정(교체)
  * PUT /api/posts/:id 
  * { title, body }
  */
-exports.replace = ctx => {
+export const replace = ctx => {
     // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용한다. 
     const { id } = ctx.params; 
     // 해당 id를 가진 posts가 몇 번째인지 확인한다. 
@@ -101,24 +103,22 @@ exports.replace = ctx => {
  * PATCH /api/posts/:id 
  * { title, body }
  */
-exports.update = ctx => {
+export const update = async ctx => {
     // PATCH 메서드는 주어진 필드만 교체한다. 
     const { id } = ctx.params; 
     // 해당 id를 가진 post가 몇 번째인지 확인한다. 
-    const index = posts.findIndex(p => p.id.toString() === id); 
-    // 포스트가 없으면 오류를 반환한다. 
-    if (index === -1) {
-        ctx.status = 404; 
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.'
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true, // 이 값을 설정하면 업데이트된 데이터를 반환한다. 
+            // false 일 때는 업데이트되기 전의 데이터를 반환한다. 
+        }).exec(); 
+        if (!post) {
+            ctx.status = 404; 
+            return;
         }
-        return; 
+        ctx.body = post; 
+    } catch(e) {
+        ctx.throw(500, e)
     }
-    // 기존 값에 정보를 덮어 씌웁니다. 
-    posts[index] = {
-        ...posts[index], 
-        ...ctx.request.body, 
-    }
-    ctx.body = posts[index]
 }
 
