77897d9e976c631f2aed6e54d94af8f2423e444c
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-07 09:06:28 +0900

    Add ObjectId verification

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,17 @@
 
 import Post from '../../models/post';
+import mongoose from 'mongoose'; 
+
+const { ObjectId } = mongoose.Types; 
+
+export const checkObjectId = ( ctx, next ) => {
+    const { id } = ctx.params; 
+    if ( !ObjectId.isValid(id)) {
+        ctx.status = 400; //Bad Request 
+        return; 
+    }
+    return next(); 
+}
 
 let postId = 1; //id의 초깃값
 
