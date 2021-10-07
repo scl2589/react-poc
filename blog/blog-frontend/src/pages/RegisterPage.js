edf8f702a7128a2acd56d016f95fcfad7bfa0d11
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-07 16:49:03 +0900

    Add front - signup and login

--- blog/blog-frontend/src/pages/RegisterPage.js
+++ blog/blog-frontend/src/pages/RegisterPage.js
@@ -1,7 +1,14 @@
-import React from 'react'; 
+import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+// import AuthForm from '../components/auth/AuthForm';
+import RegisterForm from '../containers/auth/RegisterForm';
 
 const RegisterPage = () => {
-    return <div>회원가입</div>
-}
+  return (
+    <AuthTemplate>
+      <RegisterForm />
+    </AuthTemplate>
+  );
+};
 
 export default RegisterPage;
