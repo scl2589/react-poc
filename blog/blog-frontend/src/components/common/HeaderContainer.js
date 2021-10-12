d4a07742325366b797aad825608f5372da6ea1bc
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-12 13:00:24 +0900

    Add frontend - logout

--- blog/blog-frontend/src/components/common/HeaderContainer.js
+++ blog/blog-frontend/src/components/common/HeaderContainer.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import Header from './Header';
+import { logout } from '../../modules/user';
 
 const HeaderContainer = () => {
   const { user } = useSelector(({ user }) => ({ user: user.user }));
-  return <Header user={user} />;
+  const dispatch = useDispatch();
+  const onLogout = () => {
+    dispatch(logout());
+  };
+  return <Header user={user} onLogout={onLogout} />;
 };
 
 export default HeaderContainer;
