897d94da689ba39564428bba5aaff72e593aad59
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-12 16:50:01 +0900

    Add frontend - write redux actions

--- blog/blog-frontend/src/components/common/HeaderContainer.js
+++ blog/blog-frontend/src/components/common/HeaderContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import styled from 'styled-components';
 import Header from './Header';
 import { logout } from '../../modules/user';
 
