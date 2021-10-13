65ef4821a9618f5646cb594151595bf7e14f185b
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-13 10:44:47 +0900

    Add frontend - write post

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import TagBox from '../components/write/TagBox';
-import WriteActionButtons from '../components/write/WriteActionButtons';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
-      <TagBox />
-      <WriteActionButtons />
+      <TagBoxContainer />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
