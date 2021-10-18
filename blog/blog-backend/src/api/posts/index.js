109040c52a71155a6ba81ac8a91eb9daa22145e8
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-18 15:33:09 +0900

    Add backend - html filtering

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,18 +1,18 @@
-import Router from 'koa-router'; 
-import * as postsCtrl from './posts.ctrl'
-import checkLoggedIn from '../../lib/checkLoggedIn'
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
-const posts = new Router(); 
+const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', checkLoggedIn, postsCtrl.write);
 
-const post = new Router() // /api/posts/:id 
+const post = new Router(); // /api/posts/:id
 
 post.get('/', postsCtrl.read);
-post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost,  postsCtrl.remove);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
 post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.getPostById, post.routes());
 
-export default posts
+export default posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,235 +1,265 @@
-
 import Post from '../../models/post';
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
 
-const { ObjectId } = mongoose.Types; 
+const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = ( ctx, next ) => {
-    const { id } = ctx.params; 
-    if ( !ObjectId.isValid(id)) {
-        ctx.status = 400; //Bad Request 
-        return; 
+const sanitizeOption = {
+  allowedTags: [
+    'h1',
+    'h2',
+    'b',
+    'i',
+    'u',
+    's',
+    'p',
+    'ul',
+    'ol',
+    'li',
+    'blockquote',
+    'a',
+    'img',
+  ],
+  allowedAttributes: {
+    a: ['href', 'name', 'target'],
+    img: ['src'],
+    li: ['class'],
+  },
+  allowedSchemes: ['data', 'http'],
+};
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; //Bad Request
+    return;
+  }
+  return next();
+};
+
+// 포스트 수정 및 삭제 시 권한 확인
+export const getPostById = async (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; //Bad Request
+    return;
+  }
+  try {
+    const post = await Post.findById(id);
+    // 포스트가 존재하지 않을 떄
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
     }
-    return next(); 
-}
-
-// 포스트 수정 및 삭제 시 권한 확인 
-export const getPostById = async ( ctx, next ) => {
-    const { id } = ctx.params; 
-    if ( !ObjectId.isValid(id)) {
-        ctx.status = 400; //Bad Request 
-        return; 
-    }
-    try {
-        const post = await Post.findById(id); 
-        // 포스트가 존재하지 않을 떄 
-        if (!post) {
-            ctx.status = 404 // Not Found 
-            return; 
-        }
-        ctx.state.post = post; 
-        return next(); 
-    } catch(e) {
-        ctx.throw(500, e);
-    }
-}
-
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 let postId = 1; //id의 초깃값
 
-//posts 배열 초기 데이터 
+//posts 배열 초기 데이터
 const posts = [
-    {
-        id: 1, 
-        title: '제목',
-        body: '내용'
-    }
-]
+  {
+    id: 1,
+    title: '제목',
+    body: '내용',
+  },
+];
 
 /* 포스트 작성 
 POST /api/posts 
 { title, body }
-*/ 
+*/
 
-export const write = async ctx => {
-    const schema = Joi.object().keys({
-        // 객체가 다음 필드를 가지고 있음을 검증 
-        title: Joi.string().required(), //required()가 있으면 필수 항목 
-        body: Joi.string().required(), 
-        tags: Joi.array()
-            .items(Joi.string())
-            .required(), //문자열로 이루어진 배열 
-    })
+export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), //required()가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(), //문자열로 이루어진 배열
+  });
 
-    // 검증하고 나서 검증 실패인 경우 에러 처리 
-    const result = schema.validate(ctx.request.body); 
-    if (result.error) {
-        ctx.status = 400 // Bad request
-        ctx.body = result.error; 
-        return; 
-    }
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; // Bad request
+    ctx.body = result.error;
+    return;
+  }
 
-    //REST API의 Request Body는 ctx.request.body에서 조회할 수 있다. 
-    const { title, body, tags } = ctx.request.body; 
-    const post = new Post({ 
-        title, 
-        body, 
-        tags, 
-        user:ctx.state.user 
-    });
-    try {
-        await post.save(); 
-        ctx.body = post; 
-    } catch(e) {
-        ctx.throw(500, e)
-    }
-}
+  //REST API의 Request Body는 ctx.request.body에서 조회할 수 있다.
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body: sanitizeHtml(body, sanitizeOption),
+    tags,
+    user: ctx.state.user,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-export const list = async ctx => {
-    // query는 문자열이기 때문에 숫자로 변환 필요 
-    // 값이 주어지지 않았다면 1을 기본으로 사용 
-    const page = parseInt(ctx.query.page || '1', 10);
+// html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten = (body) => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
 
-    if (page < 1) {
-        ctx.status = 400; 
-        return 
-    }
+export const list = async (ctx) => {
+  // query는 문자열이기 때문에 숫자로 변환 필요
+  // 값이 주어지지 않았다면 1을 기본으로 사용
+  const page = parseInt(ctx.query.page || '1', 10);
 
-    const { tag, username } = ctx.query; 
-    //tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음 
-    const query = {
-        ...(username ? { 'user.username': username} : {}),
-        ...(tag ? { tags: tag} : {})
-    }
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
 
-    try {
-        const posts = await Post.find(query)
-            .sort({ _id: -1 })
-            .limit(10)
-            .skip((page - 1) * 10)
-            .exec(); 
-        const postCount = await Post.countDocuments(query).exec(); 
-        ctx.set('Last-Page', Math.ceil(postCount / 10))
-        ctx.body = posts 
-            .map(post => post.toJSON())
-            .map(post => ({
-                ...post, 
-                body:
-                    post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
-            }) )
-    } catch(e) {
-        ctx.throw(500, e);
-    }
-}
+  const { tag, username } = ctx.query;
+  //tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
+  try {
+    const posts = await Post.find(query)
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
+    const postCount = await Post.countDocuments(query).exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    ctx.body = posts
+      .map((post) => post.toJSON())
+      .map((post) => ({
+        ...post,
+        body: removeHtmlAndShorten(post.body),
+      }));
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /* 특정 포스트 조회
 GET /api/posts/:id
 */
-export const read = async ctx => {
-    ctx.body = ctx.state.post;
-    // const { id } = ctx.params;
-    // try {
-    //     const post = await Post.findById(id).exec(); 
-    //     // 포스트가 없으면 올류를 반환한다. 
-    //     if (!post) {
-    //         ctx.status = 404; 
-    //         return; 
-    //     }
-    //     ctx.body = post 
-    // } catch(e) {
-    //     ctx.throw(500, e)
-    // }
-}
-
+export const read = async (ctx) => {
+  ctx.body = ctx.state.post;
+  // const { id } = ctx.params;
+  // try {
+  //     const post = await Post.findById(id).exec();
+  //     // 포스트가 없으면 올류를 반환한다.
+  //     if (!post) {
+  //         ctx.status = 404;
+  //         return;
+  //     }
+  //     ctx.body = post
+  // } catch(e) {
+  //     ctx.throw(500, e)
+  // }
+};
 
 /**
- * 특정 포스트 제거 
- * DELETE /api/posts/:id 
+ * 특정 포스트 제거
+ * DELETE /api/posts/:id
  */
-export const remove = async ctx => {
-    const { id } = ctx.params; 
-    try {
-        await Post.findByIdAndRemove(id).exec(); 
-        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
-    } catch(e) {
-        ctx.throw(500, e) 
-    }
-}
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /** 포스트 수정(교체)
- * PUT /api/posts/:id 
+ * PUT /api/posts/:id
  * { title, body }
  */
-export const replace = ctx => {
-    // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용한다. 
-    const { id } = ctx.params; 
-    // 해당 id를 가진 posts가 몇 번째인지 확인한다. 
-    const index = posts.findINdex(p => p.id.toString() === id) 
-        // 포스트가 없으면 오류를 반환한다. 
-        if (index == -1) {
-            ctx.status = 404; 
-            ctx.body = {
-                message: '포스트가 존재하지 않습니다.'
-            }
-            return 
-        }
-        // 전체 객체를 덮어 씌운다. 
-        // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만든다. 
-        posts[index] = {
-            id, 
-            ...ctx.request.body, 
-        }
-        ctx.body = posts[index]
-}
-
+export const replace = (ctx) => {
+  // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용한다.
+  const { id } = ctx.params;
+  // 해당 id를 가진 posts가 몇 번째인지 확인한다.
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  // 포스트가 없으면 오류를 반환한다.
+  if (index == -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  // 전체 객체를 덮어 씌운다.
+  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만든다.
+  posts[index] = {
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
 
 /**
- * 포스트 수정 (특정 필드 변경) 
- * PATCH /api/posts/:id 
+ * 포스트 수정 (특정 필드 변경)
+ * PATCH /api/posts/:id
  * { title, body }
  */
-export const update = async ctx => {
-    // PATCH 메서드는 주어진 필드만 교체한다. 
-    const { id } = ctx.params; 
-    // write에서 사용한 schema와 비슷한데, required()가 없다. 
-    const schema = Joi.object().keys({
-        title: Joi.string(), 
-        body: Joi.string(), 
-        tags: Joi.array().items(Joi.string())
-    })
+export const update = async (ctx) => {
+  // PATCH 메서드는 주어진 필드만 교체한다.
+  const { id } = ctx.params;
+  // write에서 사용한 schema와 비슷한데, required()가 없다.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
 
-    // 검증하고 나서 검증 실패인 경우 에러 처리 
-    const result = schema.validate(ctx.request.body); 
-    if (result.error) {
-        ctx.status = 400; //Bad Request 
-        ctx.body = result.error; 
-        return; 
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; //Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+  const nextData = { ...ctx.request.body }; // 객체를 복사하고
+  //body 값이 주어졌으면 HTML 필터링
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
+  }
+
+  // 해당 id를 가진 post가 몇 번째인지 확인한다.
+  try {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환한다.
+      // false 일 때는 업데이트되기 전의 데이터를 반환한다.
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
     }
-
-    // 해당 id를 가진 post가 몇 번째인지 확인한다. 
-    try {
-        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
-            new: true, // 이 값을 설정하면 업데이트된 데이터를 반환한다. 
-            // false 일 때는 업데이트되기 전의 데이터를 반환한다. 
-        }).exec(); 
-        if (!post) {
-            ctx.status = 404; 
-            return;
-        }
-        ctx.body = post; 
-    } catch(e) {
-        ctx.throw(500, e)
-    }
-}
-
-
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 export const checkOwnPost = (ctx, next) => {
-    const { user, post } = ctx.state; 
-    if (post.user._id.toString() !== user._id) {
-        ctx.status = 403; 
-        return; 
-    }
-    return next();
-}
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
+  return next();
+};
