5fccd5ec992b1fdf25f04e80c3ccef3e0bf47b9d
Chae Lin Shin <chaelinshin96@gmail.com>
2021-10-18 09:59:23 +0900

    Fix frontend - detailed post page

---
+++ blog/blog-frontend/src/containers/post/PostListContainer.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import qs from 'qs';
+import { withRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import PostList from '../../components/posts/PostList';
+import { listPosts } from '../../modules/posts';
+import styled from 'styled-components';
+import post from '../../modules/post';
+
+const PostListContainer = ({ location, match }) => {
+  const dispatch = useDispatch();
+  const { posts, error, loading, user } = useSelector(
+    ({ posts, loading, user }) => ({
+      posts: posts.posts,
+      error: posts.error,
+      loading: loading['posts/LIST_POSTS'],
+      user: user.user,
+    }),
+  );
+  useEffect(() => {
+    const { username } = match.params;
+  });
+};
+export default PostListContainer;
